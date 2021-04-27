073306085b5542cb3721b4720842e7f29df2b283
Lucas-Vinicius-Santos <lucasparaipaba113@gmail.com>
2021-04-27 09:13:00 -0300

    Added page home

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -3,10 +3,12 @@ import { RouterModule, Routes } from '@angular/router';
 
 import { LoginComponent } from './login/login.component';
 import { SingupComponent } from './singup/singup.component';
+import { HomeComponent } from './home/home.component';
 
 const routes: Routes = [
   { path: "login", component: LoginComponent },
   { path: "signup", component: SingupComponent},
+  { path: "home", component: HomeComponent },
   { path: "", redirectTo: '/login', pathMatch: 'full' }
 ];
 
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,22 +4,28 @@ import { BrowserModule } from '@angular/platform-browser';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import { AngularTiltModule } from 'angular-tilt';
 
-import { LoginComponent } from './login/login.component';
 
+// Angular Material
 import { MatFormFieldModule } from '@angular/material/form-field';
 import { MatIconModule } from '@angular/material/icon';
 import { MatInputModule } from '@angular/material/input';
 import { MatSliderModule } from '@angular/material/slider';
 import { MatButtonModule } from '@angular/material/button';
+
+// Components
+import { LoginComponent } from './login/login.component';
 import { SingupComponent } from './singup/singup.component';
+import { HomeComponent } from './home/home.component';
 
  
 @NgModule({
   declarations: [
     AppComponent,
     LoginComponent,
-    SingupComponent
+    SingupComponent,
+    HomeComponent
   ],
   imports: [
     BrowserModule,
@@ -30,6 +36,7 @@ import { SingupComponent } from './singup/singup.component';
     MatSliderModule,
     MatInputModule,
     MatButtonModule,
+    AngularTiltModule,
   ],
   providers: [],
   bootstrap: [AppComponent]
